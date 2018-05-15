0b65fa3dc1b4a34ed40f3426eb84fdb34379da35
Riccardo Zoncada <riccardo.zoncada@zoncrd>
2018-05-15 16:50:14 +0200

    Updated pom file with new tomcat embedded dependencies, updated gitignore with new project logic folder to be ignored and commented old code which will be refactored soon

---
+++ src/main/java/DatabaseManagement/UserRepository.java
@@ -0,0 +1,11 @@
+//package DatabaseManagement;
+//
+//import org.springframework.data.repository.CrudRepository;
+//import DatabaseManagement.User;
+//
+//// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+//// CRUD refers Create, Read, Update, Delete
+//
+//public interface UserRepository extends CrudRepository<User, Long> {
+//
+//}
--- src/main/java/RestService/GameController.java
+++ src/main/java/RestService/GameController.java
@@ -1,28 +1,44 @@
-package RestService;
-
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@CrossOrigin(value = "*")
-@RequestMapping(path="/api/v1/game")
-public class GameController {
-
-    // Crete new game
-    @PostMapping(path="/create")
-    public @ResponseBody String createNewGame(){
-        return "Miao";
-    }
-
-    // Save game
-    @PostMapping(path="/save")
-    public @ResponseBody String saveGame(){
-        return "Bau";
-    }
-
-    // Join a new game
-    @GetMapping(path="/{game_id}/join")
-    public @ResponseBody String joinNewGame(@PathVariable("game_id") long game_id){
-        return "Grrrr";
-    }
-
-}
+//package RestService;
+//
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@CrossOrigin(value = "*")
+//@RequestMapping(path="/api/v1/game")
+//public class GameController {
+//
+//    // Crete new game request
+//    @PostMapping(path="/create")
+//    public @ResponseBody String createNewGameRequest(@RequestBody GameRequest req){
+//        // add req to matching queue
+//        return req.getId();
+//    }
+//
+//    // Crete new game request
+//    @GetMapping(path="/create/{req_id}")
+//    public @ResponseBody String waitingForMatch(@PathVariable("req_id") long req_id){
+//        // wait matching thread
+//        // create new game and respond
+//        // if timeout is over, continue matching but cache the result instead to return it
+//        Game newGame = new Game();
+//        return newGame.getId();
+//    }
+//
+//    // Create new game
+//    public Game newGame createNewGame(){
+//        return;
+//    }
+//
+//    // Save game
+//    @PostMapping(path="/{game_id}/save")
+//    public @ResponseBody String saveGame(@PathVariable("game_id") long game_id){
+//
+//    }
+//
+//    // Join a new game after a match is find
+//    @PostMapping(path="/{game_id}/join")
+//    public @ResponseBody String joinNewGame(@PathVariable("game_id") long game_id){
+//        // register the user to the game
+//    }
+//
+//}
