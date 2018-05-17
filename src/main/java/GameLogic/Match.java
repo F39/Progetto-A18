b4fb864a191f76d97612c8039da6fd7a93e6690f
Riccardo Zoncada <riccardo.zoncada@zoncrd>
2018-05-18 01:50:10 +0200

    Updated with observer pattern logic, it will need soon a major refactor

--- src/main/java/GameLogic/Match.java
+++ src/main/java/GameLogic/Match.java
@@ -1,29 +1,58 @@
 package GameLogic;
 
-import People.*;
+import People.Player;
+import People.User;
+import Utils.Observer;
 
-/**Game controller. It handles a match*/
+import java.util.ArrayList;
+import java.util.List;
+
+/**It handles a match*/
 public class Match {
+
     private Board board;
     private Mode modality;
     private long timer, delta;
     private Player player1, player2;
     private PlayingRoutine routine;
     private int winner;
+    private List<Observer> observers = new ArrayList<>();
+    private int state;
+
+    public int getState() {
+        // to be updated with the game state
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+        notifyAllObservers();
+    }
+
+    public void attach(Observer observer){
+        observers.add(observer);
+    }
+
+    public void notifyAllObservers(){
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
 
     /**
      * Create a new game specifying the board dimensions, the modality and the two players
      */
-    public Match(int l, int h, Mode mode, User player1, User player2) {
+    // TODO : discriminate game mode with multiple constructor
+    public Match(int l, int h, Mode mode, User user1, User user2) {
         board = new Board(l, h);
         modality = mode;
-        timer = 0L;
-        this.player1 = new Player(player1, 1);
-        this.player2 = new Player(player2, 2);
+        timer = 0;
+        this.player1 = new Player(user1, 1);
+        this.player2 = new Player(user2, 2);
         switch (mode){
-            case MultiPlayer: routine=new MultiPlayerRoutine(this); break;
-            case SinglePlayerLevel1: routine=new AIRoutine(this, 4,-3,2,-1); break;
-            case SinglePlayerLevel2: routine=new AIRoutine(this, 5,-4,3,-1); break;
+            case MultiPlayer: routine = new MultiPlayerRoutine(this); break;
+            case SinglePlayerLevel1: routine = new AIRoutine(this, 4,-3,2,-1); break;
+            case SinglePlayerLevel2: routine = new AIRoutine(this, 5,-4,3,-1); break;
         }
     }
 
@@ -105,4 +134,5 @@ public class Match {
     public void setWinner(int winner) {
         this.winner = winner;
     }
+
 }
