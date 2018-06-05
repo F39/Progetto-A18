4930460cecc84ce011f48be0985230aa9d5236b6
xenonlcs <andyfrenz@gmail.com>
2018-06-05 18:41:04 +0200

    Token not stored in db anymore

--- src/main/java/Controllers/UserController.java
+++ src/main/java/Controllers/UserController.java
@@ -10,8 +10,11 @@ import com.j256.ormlite.support.ConnectionSource;
 import org.json.JSONObject;
 
 
-
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -19,8 +22,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.sql.SQLException;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 
@@ -29,7 +36,7 @@ public class UserController {
 
     private UserRepositoryInt userRepository;
     private EmailAdapter email;
-    private static HashMap<String, User> online = new HashMap<>(); // map sessions to relative users
+    private static Map<String, User> online = new HashMap<>(); // map sessions to relative users
 
 
     public UserController() {
@@ -55,8 +62,10 @@ public class UserController {
         String confirmLink = generateAuthToken();
         user.setEmail_token(confirmLink);
         if (addUser(user)) {
-            // TODO : Add 2 camps on db. Confirmation code and confirmation boolean.
-            String url = "http://localhost:8080/rest/user/confirm/"; //TODO: prendere la stringa dinamicamente
+             //TODO: prendere la stringa dinamicamente
+            String url = getServerURL();
+            url = url + "/rest/user/confirm/";
+
 
             email.sendEmail(user.getEmail(), null, "Confirmation email for connect4", "Press this link to confirm your registration: "+ url + confirmLink);
             return Response.status(Status.OK).build();
@@ -73,13 +82,11 @@ public class UserController {
             toConfirm.setEmail_confirmed(true);
             userRepository.updateUserEmailConfirmed(toConfirm);
 
-            File f = new  File("connect4WebApp" +  File.separator + "emailconfirmed.html");
+            File f = new File("connect4WebApp" + File.separator + "emailconfirmed.html");
 
             return new FileInputStream(f);
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
+        }catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         return null;
@@ -90,17 +97,15 @@ public class UserController {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response login(User user) {
-        try {
-            if ((user = userRepository.checkUserCredential(user.getUsername(), user.getPassword())) != null) {
-                String newToken = generateAuthToken();
-                userRepository.updateUserAuthToken(newToken, user.getUsername());
-                user.setToken(newToken);
-                online.put(newToken, user);
-                return Response.ok(new JSONObject("{\"token\":\"" + user.getToken() + "\"}").toString(), MediaType.APPLICATION_JSON).build();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+
+        if ((user = userRepository.checkUserCredential(user.getUsername(), user.getPassword())) != null) {
+            String newToken = generateAuthToken();
+            userRepository.updateUserAuthToken(newToken, user.getUsername());
+            user.setToken(newToken);
+            online.put(newToken, user);
+            return Response.ok(new JSONObject("{\"token\":\"" + user.getToken() + "\"}").toString(), MediaType.APPLICATION_JSON).build();
         }
+
         return Response.status(Status.BAD_REQUEST).entity("Login failed: the provided credentials are not valid ones.").build();
     }
 
@@ -112,22 +117,17 @@ public class UserController {
         // TODO : Set token as null
         online.remove(user.getToken());
 
-        try {
-            userRepository.updateUserAuthToken(null, user.getUsername());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        userRepository.updateUserAuthToken(null, user.getUsername());
+
         return Response.ok().build();
     }
 
     private boolean addUser(User user) {
-        try {
-            userRepository.create(user);
+        if(userRepository.create(user)){
             return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
         }
+        return false;
+
     }
 
     private String generateAuthToken() {
@@ -135,13 +135,19 @@ public class UserController {
         return authToken.toString();
     }
 
-    public static HashMap<String, User> getOnline(){
+    private String getServerURL() {
+        return "http://localhost:8080";
+    }
+
+    public static Map<String, User> getOnline() {
         return online;
     }
+
+
+    //        User newAuthUser;
+    ////        if (user.get) {
+    ////            return true;
 //    private boolean checkAuthToken(User user) {
-//        User newAuthUser;
-////        if (user.get) {
-////            return true;
 ////        } else {
 //            try {
 //                if ((newAuthUser = userRepository.getUserByAuthToken(user.getToken())) != null) {
